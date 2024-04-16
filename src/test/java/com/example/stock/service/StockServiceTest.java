7e2199a277743b7761516661eab9b83fb74c6cbe
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-04-16 22:40:38 +0900

    feat: 동시에 요청이 들어오는 테스트 코드 작성

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -37,5 +41,32 @@ class StockServiceTest {
         Stock stock = stockRepository.findById(1L).orElseThrow();
 
         assertEquals(99, stock.getQuantity());
+    }
+
+    @Test
+    public void 동시에_100개의_요청() throws InterruptedException {
+        int threadCount = 100;
+
+        // 멀티쓰레드를 사용하기위해 Excutors 사용
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        // 다른 쓰레드에서 수행중인 작업이 끝날때 까지 대기하도록 도와줌
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        // 100 - (1 * 100) = 0
+        assertEquals(0, stock.getQuantity());
+
     }
 }
