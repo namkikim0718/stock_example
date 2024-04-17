5ed5b25aaefe54fa8db7b9e89fb1005c4e6124f9
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-04-17 15:08:16 +0900

    Refactor: synchronized 사용으로 변경

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -4,17 +4,16 @@ import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class StockService {
 
     private final StockRepository stockRepository;
 
-    @Transactional
-    public void decrease(Long id, Long quantity) {
+    //@Transactional
+    public synchronized void decrease(Long id, Long quantity) {
         /**
          * Stock 재고 조회
          * 재고를 감소한 뒤 갱신된 값 저장
