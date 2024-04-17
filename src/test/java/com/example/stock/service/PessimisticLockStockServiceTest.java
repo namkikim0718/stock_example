4c06f43f581694c37c1d1d2708ab404f4d6ddeef
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-04-17 15:49:39 +0900

    feat: PessimisticLock 적용

--- src/test/java/com/example/stock/service/PessimisticLockStockServiceTest.java
+++ src/test/java/com/example/stock/service/PessimisticLockStockServiceTest.java
@@ -2,7 +2,6 @@ package com.example.stock.service;
 
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,26 +12,25 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-class StockServiceTest {
+class PessimisticLockStockServiceTest {
 
     @Autowired
-    private StockService stockService;
+    private PessimisticLockStockService stockService;
 
     @Autowired
     private StockRepository stockRepository;
 
     @BeforeEach
     public void before() {
-        Stock stock = new Stock(1L, 100L);
-        stockRepository.saveAndFlush(stock);
+        stockRepository.save(new Stock(1L, 100L));
     }
 
     @AfterEach
     public void after() {
-        Stock stock = stockRepository.findById(1L).orElseThrow();
-        stockRepository.delete(stock);
+        stockRepository.deleteAll();
     }
 
     @Test
@@ -42,7 +40,7 @@ class StockServiceTest {
         // 100 - 1 = 99
         Stock stock = stockRepository.findById(1L).orElseThrow();
 
-        Assertions.assertThat(stock.getQuantity()).isEqualTo(99);
+        assertEquals(99, stock.getQuantity());
     }
 
     @Test
@@ -68,7 +66,7 @@ class StockServiceTest {
 
         Stock stock = stockRepository.findById(1L).orElseThrow();
         // 100 - (1 * 100) = 0
-        Assertions.assertThat(stock.getQuantity()).isEqualTo(0);
+        assertEquals(0, stock.getQuantity());
 
     }
 }
